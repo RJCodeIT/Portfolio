980d9b88eb815fecd610999210f37f6b12ee7dcc
Jakub Kliś <jakubklis2201@gmail.com>
2025-01-26 18:16:18 +0100

    feat: Add projects section

---
+++ rj-code-portfolio/src/const/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Sneaker Finder",
+    des: "Sneaker Finder website",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "My Parish",
+    des: "My Parish website",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Pawnshop",
+    des: "Pawnshop website",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Alpacarnia",
+    des: "Alpacarnia website",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
--- rj-code-portfolio/src/containers/ProjectsContainer.tsx
+++ rj-code-portfolio/src/containers/ProjectsContainer.tsx
@@ -1,9 +1,113 @@
-const ProjectsContainer = () => {
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+import Image from "next/image";
+import { projects } from "../const/projects";
+import { PinContainer } from "../components/ui/Pin";
+import Button from "../components/ui/Button";
+
+export default function ProjectsContainer() {
   return (
-    <section id="projects" className="min-h-screen">
-      <h1>ProjectsContainer</h1>
+    <section id="projects">
+    <div className="py-20 container mx-auto px-4">
+      <div className="text-center mb-24">
+        <h2 className="text-4xl font-bold mb-4">Our Latest Work</h2>
+        <p className="text-gray-400 text-lg max-w-2xl mx-auto">
+          Discover the projects we&apos;ve built to empower businesses with cutting-edge web solutions.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-[90%] mx-auto">
+        {projects.map((item) => (
+          <div
+            className="min-h-[32rem] w-full"
+            key={item.id}
+          >
+            <PinContainer
+              title={item.title}
+              href={item.link}
+              className="w-full"
+              containerClassName="w-full"
+            >
+              <div className="relative flex items-center justify-center w-full overflow-hidden h-[25vh] lg:h-[35vh] mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <Image 
+                    src="/bg.png" 
+                    alt="bgimg"
+                    fill
+                    className="object-cover"
+                    priority
+                  />
+                </div>
+                <Image
+                  src={item.img}
+                  alt="cover"
+                  width={400}
+                  height={300}
+                  className="z-10 absolute bottom-0"
+                  priority
+                />
+              </div>
+
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {item.title}
+              </h1>
+
+              <p
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
+                {item.des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <Image 
+                        src={icon} 
+                        alt={`technology-${index}`}
+                        width={24}
+                        height={24}
+                        className="p-2"
+                      />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+
+      <div className="text-center">
+        <p className="text-xl text-gray-400 mb-6">
+          Want to collaborate with us? Let&apos;s bring your ideas to life!
+        </p>
+        <Button variant="primary" size="lg">
+          Contact Us
+        </Button>
+      </div>
+    </div>
     </section>
   );
 };
-
-export default ProjectsContainer;
