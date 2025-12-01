83aaa95f9fccf3b5c0ef8fd36610c6e475c626f9
Jakub Kliś <jakubklis2201@gmail.com>
2025-12-01 09:00:27 +0100

    feat: Add Budgenix to projects list

--- rj-code-portfolio/src/const/projects.ts
+++ rj-code-portfolio/src/const/projects.ts
@@ -16,23 +16,30 @@ export const projects: Project[] = [
   },
   {
     id: 2,
+    key: "budgenix",
+    img: "/budgenix.webp",
+    iconLists: ["/nextjs.svg", "/typescript.svg", "/tailwind.svg", "/vercel.svg", "/microsoftSqlServer.svg"],
+    link: "https://rjcode-portfolio.pl/budgenix",
+  },
+  {
+    id: 3,
     key: "myParish",
     img: "/my-parish.webp",
     iconLists: ["/nextjs.svg", "/typescript.svg", "/tailwind.svg", "/vercel.svg", "/microsoftSqlServer.svg"], 
     link: "https://rjcode-portfolio.pl/mojaParafia",
   },
-  {
-    id: 3,
-    key: "pawnshop",
-    img: "/pawnshop.webp",
-    iconLists: ["/react.svg", "/tailwind.svg", "/typescript.svg", "/vite.svg", "/microsoftSqlServer.svg"],
-    link: "https://rjcode-portfolio.pl/pawnshop",
-  },
-  {
+    {
     id: 4,
     key: "usedcars",
     img: "/used-cars.webp",
     iconLists: ["/react.svg", "/tailwind.svg", "/typescript.svg", "/vite.svg", "/mongodb.svg"],
     link: "https://rjcode-portfolio.pl/usedcars",
+  },
+  {
+    id: 5,
+    key: "pawnshop",
+    img: "/pawnshop.webp",
+    iconLists: ["/react.svg", "/tailwind.svg", "/typescript.svg", "/vite.svg", "/microsoftSqlServer.svg"],
+    link: "https://rjcode-portfolio.pl/pawnshop",
   },
 ];
